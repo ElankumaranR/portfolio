a4e5b07bb163a18d53846a037e2d126e0d99107a
ELANKUMARANR <elankumaran2103@gmail.com>
2024-07-29 00:22:47 +0530

    -m
    "Fix Babel dependency and ESLint warnings"

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -8,7 +8,7 @@ function Project(){
             <h1 className="proj_tit">Projects</h1>
             <div className="Proj_content">
                 <div className="proj_comp">
-                    <img src="ecom.jpg" class="proj_img"/>
+                    <img src="ecom.jpg" class="proj_img" alt="p1"/>
                     <div className="proj_desc">
                         <h3>Ecommerce</h3>
                         <p>it is a project of Ecommerce website done by python flask</p>
@@ -16,14 +16,14 @@ function Project(){
                     <a href="https://github.com/ElankumaranR/ecomerce" target="_blank" rel="noopenner noreferrer" className="but">View on GitHub</a>
                 </div>
                 <div className="proj_comp">
-                    <img src="emp.png" class="proj_img"/>
+                    <img src="emp.png" alt="p2" class="proj_img"/>
                     <div className="proj_desc">
                         <h3>employee management</h3>
                         <p>it is a project of management website done by java spring boot</p>
                     </div>
                     <a href="https://github.com/ElankumaranR/employee_management" target="_blank" rel="noopenner noreferrer" className="but">View on GitHub</a>
                 </div><div className="proj_comp">
-                    <img src="rr.webp" class="proj_img"/>
+                    <img src="rr.webp"  alt ="p3"class="proj_img"/>
                     <div className="proj_desc">
                         <h3>Railway Reservation</h3>
                         <p>it is a project of Reservation website done by python flask</p>
--- src/components/skill.jsx
+++ src/components/skill.jsx
@@ -8,38 +8,38 @@ function Skill(){
         <div className="skill_content">
             <div className="skill_comp">
                 <div className="skill_in">
-                <img src="js.png" className="skill_png"/>
+                <img src="js.png" alt="s" className="skill_png"/>
                 <p className="skill_per">89%</p>
                 </div>
                 <p className="skill_name">Javascript</p>
             </div>
             <div className="skill_comp">
                 <div className="skill_in">
-                <img src="sql.png" className="skill_png"/>
+                <img src="sql.png"alt="s" className="skill_png"/>
                 <p className="skill_per">85%</p>
                 </div>
                 <p className="skill_name">SQL</p>
             </div> <div className="skill_comp">
                 <div className="skill_in">
-                <img src="git.png" className="skill_png"/>
+                <img src="git.png"  alt="s"className="skill_png"/>
                 <p className="skill_per">79%</p>
                 </div>
                 <p className="skill_name">GIT</p>
             </div> <div className="skill_comp">
                 <div className="skill_in">
-                <img src="node.png" className="skill_png"/>
+                <img src="node.png" alt="s" className="skill_png"/>
                 <p className="skill_per">84%</p>
                 </div>
                 <p className="skill_name">Nodejs</p>
             </div> <div className="skill_comp">
                 <div className="skill_in">
-                <img src="react.png" className="skill_png"/>
+                <img src="react.png"  alt="s"className="skill_png"/>
                 <p className="skill_per">80%</p>
                 </div>
                 <p className="skill_name">Reactjs</p>
             </div> <div className="skill_comp">
                 <div className="skill_in">
-                <img src="flask.png" className="skill_png"/>
+                <img src="flask.png" alt="s" className="skill_png"/>
                 <p className="skill_per">80%</p>
                 </div>
                 <p className="skill_name">Flask</p>
